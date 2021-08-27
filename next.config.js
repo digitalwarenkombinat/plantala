1189d78fb226b734e7a835378eabb93ebeb5fbd4
Gerd Müller <g.mueller@ewerk.com>
2021-08-27 19:34:21 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
 
 module.exports = {
-  assetPrefix: ghPages ? '/plantala/' : '',
-  basePath: ghPages ? '/plantala' : '',
+  assetPrefix: '/plantala/',
+  basePath: '/plantala',
+  trailingSlash: true,
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' }
+    };
 };
