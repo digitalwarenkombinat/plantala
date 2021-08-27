8edfb97f3305c73a509d330ff7723bc188af605a
Gerd Müller <g.mueller@ewerk.com>
2021-08-27 18:56:01 +0200

    Adjust gh-pages workflow

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
 
 module.exports = {
-  assetPrefix: ghPages ? '/digitalwarenkombinat.github.io/plantala' : '',
-  basePath: '/plantala',
+  assetPrefix: ghPages ? '/plantala' : '',
+  basePath: ghPages ? '/plantala' : '',
 };
