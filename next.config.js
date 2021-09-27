f2564d14e67a049d41cd4c83a1e517e41a684570
Gerd Müller <g.mueller@ewerk.com>
2021-09-27 14:42:39 +0200

    Remove next configuration

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const pathPrefix = process.env.DEPLOY_TARGET === 'gh-pages' ? '/plantala' : '';
-
-module.exports = {
-  basePath: pathPrefix,
-  assetPrefix: `${pathPrefix}/`,
-  env: {
-    pathPrefix,
-  },
-};
